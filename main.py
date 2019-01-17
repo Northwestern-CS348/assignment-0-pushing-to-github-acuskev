e2876b7144013e0349433a1159097d1e41dc4b84
Kevin Qiu <acuskevq@gmail.com>
2019-01-17 15:39:45 -0600

    Initial commit

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ class SetupTest(unittest.TestCase):
 	def test01(self):
 		result = student_code.shouldReturnTrue()
 		self.assertTrue(result)
+	def test02(self):
+		result = student_code.shouldReturnTrue()
+		x = 2
+		if result:
+			x = 1
+		self.assertEqual(x, 1)
 
 if __name__ == '__main__':
 	unittest.main()
